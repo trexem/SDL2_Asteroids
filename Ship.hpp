b2af9e0a2ae3bd6d7b6c338269c77accd1aefa5f
trexem <trex_emm@live.com.mx>
2021-08-20 13:14:04 -0500

    Shoots BIG LASER BEAMS TODO:  - Asteroid class/header file  - Collision detection for asteroids  - Pause/Resume/Restart game

--- Ship.hpp
+++ Ship.hpp
@@ -2,6 +2,42 @@
 #define _SHIP_H_
 
 #include "Texture.hpp"
+const int SHIP_MAX_SHOTS = 10;
+
+class Shot {
+public:
+	const int SHOT_SPEED = 1000;
+
+	Shot(int t_x, int t_y, double t_degrees, Texture* t_texture) {
+		m_pos_x = t_x;
+		m_pos_y = t_y;
+		m_rot_degrees = t_degrees;
+		m_texture = t_texture;
+	}
+
+	void move(double t_time_step) {
+		if (!isDead()) {
+			double radians = m_rot_degrees * PI / 180;
+			m_pos_x += SHOT_SPEED * t_time_step * sin(radians);
+			m_pos_y -= SHOT_SPEED * t_time_step * cos(radians);
+		}
+	}
+	void render() {
+		if (!isDead()) {
+			m_texture->renderEx(m_pos_x, m_pos_y, nullptr, m_rot_degrees, nullptr, SDL_FLIP_NONE);
+		}
+	}
+	bool isDead() {
+		if (m_pos_x > SCREEN_WIDTH + 15 || m_pos_x < -15 || m_pos_y > SCREEN_HEIGHT + 15 || m_pos_y < -15) {
+			return true;
+		}
+		return false;
+	}
+private:
+	int m_pos_x{0}, m_pos_y{0};
+	double m_rot_degrees{0};
+	Texture* m_texture;
+};
 
 class Ship {
 public:
@@ -11,15 +47,27 @@ public:
 	const int SHIP_TOP_ROTATION_SPEED = 540; //1.5 turn per second
 	const int SHIP_SPEED = 15;
 	const int SHIP_ROT_SPEED = 10;
+	const float SHIP_SHOT_DELAY = .25;
 
-	Ship(SDL_Renderer* t_renderer) {
+	Ship(SDL_Renderer * t_renderer) {
 		m_pos_x = 0;
 		m_pos_y = 0;
 		m_rot_degrees = 0;
 		m_vel = 0;
 		m_rot_vel = 0;
-		m_texture = Texture(t_renderer);
+		m_renderer = t_renderer;
+		m_texture = Texture(m_renderer);
+		m_shot_texture = Texture(m_renderer);
 		m_texture.loadFromFile("img/spaceship.bmp");
+		m_shot_texture.loadFromFile("img/shot.png");
+		for (int i = 0; i < SHIP_MAX_SHOTS; i++) {
+			m_shots[i] = new Shot(-20, -20, 0, &m_shot_texture);
+		}
+	}
+	~Ship() {
+		for (int i = 0; i < SHIP_MAX_SHOTS; i++) {
+			delete m_shots[i];
+		}
 	}
 
 	void handleInput(const Uint8* t_current_key_states) {
@@ -77,8 +125,16 @@ public:
 			}
 			m_rot_vel += SHIP_ROT_SPEED;
 		}
+		if (t_current_key_states[SDL_SCANCODE_SPACE] && m_last_shot > SHIP_SHOT_DELAY) {
+			shoot();
+			m_last_shot = 0;
+		}
 	}
 	void move(double t_time_step) {
+		m_last_shot += t_time_step;
+		for (int i = 0; i < SHIP_MAX_SHOTS; i++) {
+			m_shots[i]->move(t_time_step);
+		}
 		    //convert degrees to radians
 		double radians = m_rot_degrees * PI / 180;
 		    //top max and min speed
@@ -123,6 +179,7 @@ public:
 		} else if (m_rot_degrees < -180) {
 			m_rot_degrees += 360;
 		}
+
 	}
 
 	void setPos(double t_x, double t_y, double t_rot_degrees) {
@@ -132,9 +189,23 @@ public:
 	}
 	void render() {
 		m_texture.renderEx(m_pos_x, m_pos_y, nullptr, m_rot_degrees, nullptr, SDL_FLIP_NONE);
+		renderShots();
+	}
+	void shoot() {
+		for (int i = 0; i < SHIP_MAX_SHOTS; i++) {
+			if (m_shots[i]->isDead()) {
+				delete m_shots[i];
+				double radians = m_rot_degrees * PI / 180;
+				int x = (int) (m_pos_x + m_texture.getWidth() / 2 + sin(radians) * m_texture.getHeight() / 2);
+				int y = (int) (m_pos_y + m_texture.getHeight() / 2 - cos(radians) * m_texture.getHeight() / 2);
+				m_shots[i] = new Shot(x, y, m_rot_degrees, &m_shot_texture);
+				i = SHIP_MAX_SHOTS;
+			}
+		}
 	}
 	void free() {
 		m_texture.free();
+		m_shot_texture.free();
 	}
 	double getX() {
 		return m_pos_x;
@@ -149,8 +220,16 @@ public:
 		return m_texture;
 	}
 private:
+	void renderShots() {
+		for (int i = 0; i < SHIP_MAX_SHOTS; i++) {
+			m_shots[i]->render();
+		}
+	}
+	Shot* m_shots[SHIP_MAX_SHOTS];
 	double m_pos_x{0}, m_pos_y{0}, m_rot_degrees{0};
 	double m_vel{0}, m_rot_vel{0};
-	Texture m_texture;
+	Texture m_texture, m_shot_texture;
+	SDL_Renderer* m_renderer;
+	double m_last_shot;
 };
 #endif
